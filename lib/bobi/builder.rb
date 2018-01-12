fc2524b68fec0213cd77144ec7e2bd3039d4a8b5
Michał Kalbarczyk <fazibear@gmail.com>
2018-01-12 18:48:09 +0100

    Fix color name

--- lib/bobi/builder.rb
+++ lib/bobi/builder.rb
@@ -24,7 +24,7 @@ class Builder
 
     config = read_config(tmp)
 
-    log(config.inspect.purple)
+    log(config.inspect.magenta)
 
     config.each do |build|
       log "Starting build #{build} ...".green
